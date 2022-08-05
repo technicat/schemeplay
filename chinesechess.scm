a5ae93ba94d74f57f13decbb777ab1f2a7ca74c2
Philip Chu <technicat@users.noreply.github.com>
2022-08-04 23:59:29 -0700

    Create chinesechess.scm

---
+++ chinesechess.scm
@@ -0,0 +1,18 @@
+(include "boardgame.scm")
+
+(define-class <chinesechess> (<gridgame>) ())
+
+(define-class <pawn> (<piece>) ())
+
+(define-class <king> (<piece>) ())
+
+(define-class <queen> (<piece>) ())
+
+(define-class <bishop> (<piece>) ())
+
+(define-class <knight> (<piece>) ())
+
+(define-class <rook> (<piece>) ())
+
+
+
