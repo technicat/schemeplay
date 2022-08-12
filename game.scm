f3df47c718f8502f5072255456ea2f3df3a4facd
Philip Chu <technicat@users.noreply.github.com>
2022-08-12 15:32:13 -0700

    Update game.scm

--- game.scm
+++ game.scm
@@ -9,11 +9,20 @@
 (define-method end? ((game <game>))
     #f)
 
+(define-method your-move ((player <player>) (game <game>)))
+
 (define-method play ((game <game>))
     (if (end? game)
         (print "Game Over")
         (begin
             (push! (turns game) (play-turn game))
             (play game))))
+
+(define-method play-round ((game <game>))
+    (let f ((playas (players game)))
+    (if (not (null? playas))
+        (let ((player (car playas)))
+            (your-move player game)
+            (f (cdr playas))))))
 
 (define-method play-turn ((game <game>)))
