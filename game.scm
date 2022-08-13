ba9c30de4f29a0b3fe642fc8d2ca296e2965b3b9
Philip Chu <technicat@users.noreply.github.com>
2022-08-12 19:17:34 -0700

    human

--- game.scm
+++ game.scm
@@ -1,19 +1,18 @@
-(define-class <game> () (
-                         (players :accessor players :init-value '())
-                         (moves :accessor turns :init-value '())))
+(define-class <game> ()
+ ((players :getter players :init-keyword :players)
+  (moves :accessor moves :init-value '())))
 
-(define-class <player> () (name))
+(define-class <player> ()
+ ((name :getter name :init-keyword :name)))
 
-(define-class <computer> () (player))
+(define-class <computer> (<player>)
+    ())
 
-(define-class <human> () (player))
+(define-class <human> (<player>)
+    ())
 
 (define-method end? ((game <game>))
  #f)
-
-(define-method your-move ((player <player>) (game <game>)))
-
-(define-method your-move ((player <computer>) (game <game>)))
 
 (define-method your-move ((player <human>) (game <game>))
  (read-line))
@@ -28,6 +27,6 @@
 (define-method play-round ((game <game>))
  (let f ((playas (players game)))
   (if (not (null? playas))
-   (let ((player (car playas)))
-    (push (moves game) (your-move player game))
+   (let ((playa (car playas)))
+    (push (moves game) (your-move playa game))
     (f (cdr playas))))))
--- guess.scm
+++ guess.scm
@@ -3,6 +3,8 @@
 (use gauche.parseopt) ; command line args
 (use srfi-27) ; random-real
 
+(include "game.scm")
+
 (define (main args)
  (let-args (cdr args)
   ((h "h|help")
@@ -13,19 +15,17 @@
    (print "guess.scm -h|help")
    (play (make <guess>
           :number (random-integer r)
-          :players (list (make <human>)))))))
-
-(include "game.scm")
+          :players (list (make <human> :name "Human")))))))
 
 (define-class <guess> (<game>)
  ((number :init-keyword :number :getter number)))
 
 (define-method your-move ((player <human>) (game <guess>))
- (print "Guess what number I'm thinking?")
+ (print #"~(name player), guess what number I'm thinking?")
  (or (string->number (next-method))
   (your-move player game)))
 
 (define-method end? ((game <guess>))
- (and (not (null? (turns game)))
+ (and (not (null? (moves game)))
   (= (car (turns)) (number game))))
 
